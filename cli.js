81513af2b41584abc13f0a36ae2c782031696509
Tim Neutkens <timneutkens@me.com>
2017-09-22 09:24:05 +0200

    Initial commit

---
+++ cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const {exec} = require('mz/child_process')
+const [command] = process.argv.slice(2)
+
+async function getBitbucketRemote() {
+  const output = await exec('git remote -v')
+  const lines = output[0].trim().split('\n')
+  for(line of lines) {
+    const [_, bitbucketRepository] = line.match(/git@bitbucket.org:(.*\/.*).git/)
+    if(bitbucketRepository) {
+      return bitbucketRepository
+    }
+  }
+}
+
+async function open(url) {
+  await exec(`open ${url}`)
+}
+
+function getBitbucketUrl(path) {
+  return `https://bitbucket.org/${path}`
+}
+
+async function run() {
+  try {
+    const repository = await getBitbucketRemote()
+    const repositoryUrl = getBitbucketUrl(repository)
+    
+    if(command === 'pipelines') {
+      return await open(`${repositoryUrl}/addon/pipelines/home`)
+    }
+
+    if(!command || command === 'browse') {
+      return await open(repositoryUrl)
+    }
+
+    console.log('No such command!')
+
+  } catch (err) {
+    if(err.message.indexOf(`fatal: No such remote 'origin'`) !== -1) {
+      console.error(`Remote 'origin' is not available`)
+      process.exit(1)
+      return
+    }
+
+    if(err.message.indexOf(`fatal: Not a git repository (or any of the parent directories): .git`) !== -1) {
+      console.error('This directory does not contain a git repository')
+      process.exit(1)
+      return
+    }
+
+    console.error(err.message)
+  }
+  
+}
+
+run()
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Bitbucket Open
+
+Easily open bitbucket.org for your current project
+
+## Installation
+
+```
+npm install -g bitbucket-open
+```
+
+## Usage
+
+The bitbucket url will be derived from the git remote url.
+
+Open project in bitbucket: 
+
+```
+bb
+```
+
+Open pipelines for project
+
+```
+bb pipelines
+```
